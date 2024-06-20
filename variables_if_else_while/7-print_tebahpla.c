39544a14c1c1974ff683cb08d20aed8406716788
Saki <saki@LeHote.myguest.virtualboxorg>
2024-06-20 14:01:23 +0200

    Reverse alphabet

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main - 
+*
+* Return: Always 0
+*/
+
+int main(void)
+{
+	int x;
+
+	for (x = 122; x >= 97; x--)
+	{
+		putchar(x);
+	}
+
+	putchar(10);
+	return (0);
+}
