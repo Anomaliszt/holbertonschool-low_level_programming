b043bf024351ae1ebd569424809f7f9327a64d89
Saki <saki@LeHote.myguest.virtualboxorg>
2024-06-20 10:59:46 +0200

    Prints 2 Alphabets

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+* main - Print 2 alphabets 
+*
+* Return: Always 0
+*/
+
+int main(void)
+{
+	int x;
+
+	for (x = 97; x <= 122; x++)
+	{
+		putchar(x);
+	}
+
+	for (x = 65; x <= 90; x++)
+	{
+		putchar(x);
+	}
+
+	putchar(10);
+	return (0);
+}
