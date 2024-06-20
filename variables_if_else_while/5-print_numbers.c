1e308eed186107a481d7e8f24cef5a87af3db839
Saki <saki@LeHote.myguest.virtualboxorg>
2024-06-20 13:50:11 +0200

    Print Numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main - 
+*
+* Return: Always 0
+*/
+
+int main(void)
+{
+	int x;
+
+	for (x = 49; x <= 57; x++)
+	{
+		putchar(x);
+	}
+
+	putchar(10);
+	return (0);
+}
