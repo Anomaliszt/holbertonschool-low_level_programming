4b5f94290667b487b3b18e1c27cb84a5f88ed447
Saki <saki@LeHote.myguest.virtualboxorg>
2024-06-21 10:01:45 +0200

    Hexadecimal

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main -
+*
+* Return: Always 0
+*/
+
+int main(void)
+{
+	int x;
+
+	for (x = 48; x <= 57; x++)
+		putchar(x);
+	for (x = 97; x <= 102; x++)
+		putchar(x);
+
+	putchar(10);
+	return (0);
+}
