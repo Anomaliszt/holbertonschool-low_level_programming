efcc0f88674d002c4289f4d5b42542498ad0c152
Saki <saki@LeHote.myguest.virtualboxorg>
2024-06-21 13:12:05 +0200

    Print alphabet x10

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* print_alphabet_x10
+* print_alphabet_x10 - Prints alphabet x10
 *
 * Return: Always 0
 */
@@ -11,7 +11,7 @@ void print_alphabet_x10(void)
 	int x;
 	int y;
 
-	for (y = 0; y <= 10; y++)
+	for (y = 0; y <= 9; y++)
 	{
 		for (x = 'a'; x <= 'z'; x++)
 		{
