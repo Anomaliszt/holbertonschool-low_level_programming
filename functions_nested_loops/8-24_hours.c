dbc254ff4217231f2eaadd994c7853e9302658e3
Saki <saki@LeHote.myguest.virtualboxorg>
2024-06-23 20:53:38 +0200

    24 hours

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+* jack_bauer - Prints every minute of the day
+*
+* Return: Always 0
+*/
+
+void jack_bauer(void)
+{
+	int hour;
+	int min;
+	int hourb;
+	int minb;
+
+	for (hour = 0; hour <= 1; hour++)
+		for (min = 0; min <= 9; min++)
+			for (hourb = 0; hourb <= 5; hourb++)
+				for (minb = 0; minb <= 9; minb++)
+				{
+				_putchar(hour + '0');
+				_putchar(min + '0');
+				_putchar(':');
+				_putchar(hourb + '0');
+				_putchar(minb + '0');
+				_putchar('\n');
+				}
+
+	for (hour = 1; hour <= 2; hour++)
+		for (min = 0; min <= 3; min++)
+			for (hourb = 0; hourb <= 5; hourb++)
+				for (minb = 0; minb <= 9; minb++)
+				{
+				_putchar(hour + '0');
+				_putchar(min + '0');
+				_putchar(':');
+				_putchar(hourb + '0');
+				_putchar(minb + '0');
+				_putchar('\n');
+				}
+}
