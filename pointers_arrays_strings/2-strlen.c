de6f5d489e8130254e250714f03fb678e234d4ac
Saki <saki@LeHote.myguest.virtualboxorg>
2024-06-24 23:08:02 +0200

    Print string number

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,12 +1,18 @@
 #include "main.h"
 
 /**
-*
-*
-*
+* _strlen - Print length of string
+* @s: random string
+* Return: returns length
 */
 
 int _strlen(char *s)
 {
-	
+	int x;
+
+	while (s[x] != '\0')
+	{
+		x++;
+	}
+	return (x);
 }
