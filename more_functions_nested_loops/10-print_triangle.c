88c5f96574e08fdb894253b50a1a60417bce7b83
Saki <saki@LeHote.myguest.virtualboxorg>
2024-06-24 16:32:42 +0200

    Prints Triangle

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+* print_triangle - Prints triangle
+* @size:
+* Return: nothing
+*/
+
+void print_triangle(int size)
+{
+	int x, y, k;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (x = 0; x < size; x++)
+		{
+			for (y = 0; y < size -x -1; y++)
+				_putchar(' ');
+			for (k = size -x -1; k < size; k++)
+				_putchar('#');
+			_putchar('\n');
+		}
+	}
+}
