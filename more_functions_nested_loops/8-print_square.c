862fbabeb56997fec54d55a0ab18c2fc5bccd24b
Saki <saki@LeHote.myguest.virtualboxorg>
2024-06-24 14:26:50 +0200

    Print square fix

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -11,11 +11,16 @@ void print_square(int size)
 	int x;
 	int y;
 
-	for (x = 0; x < size; x++)
-	{
-		for (y = 1; y < size; y++)
-			_putchar('#');
-		_putchar('#');
+	if (size <= 0)
 		_putchar('\n');
+	else
+	{
+		for (x = 0; x < size; x++)
+		{
+			for (y = 1; y < size; y++)
+				_putchar('#');
+			_putchar('#');
+			_putchar('\n');
+		}
 	}
 }
