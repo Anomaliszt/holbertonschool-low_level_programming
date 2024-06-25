91d30077b09ae2c0fa7095a4d3852c4b5094e321
Saki <saki@LeHote.myguest.virtualboxorg>
2024-06-25 12:08:24 +0200

    Print every other number

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -8,12 +8,14 @@
 
 void puts2(char *str)
 {
-	int x = 0;
+	int x;
+	int y = 0;
 
-	while (str[x])
-	{
+	while (str[y] != '\0')
+		y++;
+
+	for (x = 0 ; x < y; x += 2)
 		_putchar(str[x]);
-		x += 2;
-	}
+
 	_putchar('\n');
 }
