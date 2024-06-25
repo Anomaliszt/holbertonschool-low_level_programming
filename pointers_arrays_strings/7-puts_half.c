8f63c23c2e2df6e5fbf13bf2f4639388cb8e567b
Saki <saki@LeHote.myguest.virtualboxorg>
2024-06-25 12:24:15 +0200

    Prints half the string

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* puts_half - Prints half the string
+* @str: random string
+* Return: nothing
+*/
+
+void puts_half(char *str)
+{
+	int x;
+	int y;
+
+	while (str[y] != '\0')
+		y++;
+
+	for (x = y / 2; str[x] != '\0'; x++)
+		_putchar(str[x]);
+	_putchar('\n');
+}
