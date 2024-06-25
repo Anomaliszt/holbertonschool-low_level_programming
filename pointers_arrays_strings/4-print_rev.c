1b836d4643c8efe04d189f00a4f550571dd03995
Saki <saki@LeHote.myguest.virtualboxorg>
2024-06-25 10:17:33 +0200

    Print reverse string

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,17 +1,20 @@
 #include "main.h"
 
 /**
-*
-*
-*
+* print_rev - print reverse
+* @s: random string
+* Return: nothing
 */
 
 void print_rev(char *s)
 {
-	while (*s)
-	{
-		_putchar(*s);
-		s--;
-	}
+	int x;
+
+	while (s[x])
+		x++;
+
+	while (x--)
+		_putchar(s[x]);
 	_putchar('\n');
 }
+
