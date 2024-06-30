59dad2e0b5bdb55b405de6967ac49e0c865f254a
Anomaliszt <xavier.sanchez44@orange.fr>
2024-06-30 21:00:12 +0000

    Strncat

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strncat - function
+ * @dest: random value
+ * @src: random value
+ * @n: random value
+ * Return: dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int size_dest = 0;
+
+	while (dest[size_dest] != '\0')
+		size_dest++;
+	while (src[i] && i < n)
+	{
+		dest[size_dest + i] = src[i];
+		i++;
+	}
+	dest[size_dest + i] = '\0';
+	return (dest);
+}
