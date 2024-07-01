f70810fc361723e180a80db8a00b66354b894cc2
Anomaliszt <xavier.sanchez44@orange.fr>
2024-07-01 09:28:28 +0000

    Strncpy fix

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -10,12 +10,17 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i = 0;
+	int i;
 
-	while ((dest[i] || src[i]) && i < n)
+	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
-		dest[i] = src[i];
-		i++;
+	dest[i] = src[i];
 	}
+
+	for (; i < n; i++)
+	{
+	dest[i] = '\0';
+	}
+
 	return (dest);
 }
