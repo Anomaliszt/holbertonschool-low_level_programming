6827505a23415ce70e4a1e5a60dfb173c8fea5c2
Saki <saki@LeHote.myguest.virtualboxorg>
2024-07-02 12:13:38 +0200

    Strchr

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* *_strchr - locates char
+* @s: random char
+* @c: random char
+* Return: returns pointer or NULL
+*/
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; (s[i] != '\0'); i++)
+	{
+		if (s[i] == c)
+			return (s + i);
+		if (s[i] == '\0')
+			return (s + i);
+	}
+	return (NULL);
+}
