d32675407eaa28471b588c69a6a9fb98f13c7c8e
Saki <saki@LeHote.myguest.virtualboxorg>
2024-07-02 16:24:40 +0200

    Strspn

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+*
+*
+*
+*
+*/
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int i;
+	int ii;
+	int k = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (ii = 0; accept[ii] != '\0'; ii++)
+		{
+			if (s[i] == accept[ii])
+				k++;
+			if (s[i] <= 'A' || (s[i] >= 'Z' && s[i] <= 'a') || s[i] >= 'z')
+				return (k);
+		}
+	}
+	return (k);
+}
