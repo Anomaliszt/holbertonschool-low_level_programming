2d137d60a946bf45044841633960deb93377fd77
Anomaliszt <xavier.sanchez44@orange.fr>
2024-07-12 13:16:45 +0000

    Dog fix

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -10,8 +10,18 @@
 
 void print_dog(struct dog *d)
 {
-	if (d)
-		printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 	if (d == NULL)
-		return;
+	return;
+
+	if (d->name == NULL)
+		printf("Name: (nil)\n");
+	else
+		printf("Name: %s\n", d->name);
+
+	printf("Age: %.1f\n", d->age);
+
+	if (d->owner == NULL)
+		printf("Owner: (nil)\n");
+	else
+		printf("Owner: %s\n", d->owner);
 }
