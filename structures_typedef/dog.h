a76341fa0496148bd80cf79fce05e9a0df0ed838
Anomaliszt <xavier.sanchez44@orange.fr>
2024-07-12 11:57:24 +0000

    Dog add

---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+/**
+ * struct dog - dog stats
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
