1e3d1b6f5d4ef16abead06795bda81ea25506ccf
Anomaliszt <xavier.sanchez44@orange.fr>
2024-07-12 13:08:41 +0000

    print dog

---
+++ structures_typedef/2-print-dog.c
@@ -0,0 +1,13 @@
+#include "dog.h"
+
+/**
+ * print_dog - print dog stats
+ * @d: dog struct
+ *
+ * Return: Nothing
+ */
+void print_dog(struct dog *d)
+{
+	if (d)
+		printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+}
