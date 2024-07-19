2b39670c3153ede4b1165f815d320e5ce61eafc9
Anomaliszt <xavier.sanchez44@orange.fr>
2024-07-19 10:03:17 +0200

    Fix test

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -11,13 +11,13 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 {
     va_list args;
     unsigned int i;
-    
+
     va_start(args, n);
     
     for (i = 0; i < n; i++) 
     {
         printf("%d", va_arg(args, int));
-        
+
         if (separator != NULL && i < n - 1) 
         {
             printf("%s", separator);
@@ -25,6 +25,6 @@ void print_numbers(const char *separator, const unsigned int n, ...)
     }
     
     va_end(args);
-    
+
     printf("\n");
 }
